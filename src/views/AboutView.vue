02d5bc86d8ee48aa45425d3739962214e6ce973d
mahardikaraka <mahardika.raka@widyatama.ac.id>
2023-10-21 12:58:44 +0700

    PU: Pagination

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,37 +1,68 @@
 <template>
-  <div class="about">
-    <div v-for="(item, index) in listItems" :key="index">
-       {{item?.name}}
-     </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-pagination
+          :length="info.pages"
+          @input="fetchCharacters"
+        ></v-pagination>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-list>
+          <v-list-item v-for="character in listItems" :key="character?.id">
+            <v-list-item-content>
+              <v-list-item-title>{{ character.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import di from '../di'
 
-interface Item {
+interface Character {
+  id: number;
   name: string;
 }
 
+interface Info {
+  count: number;
+  pages: number;
+  next: string;
+  prev: string;
+}
+
 export default {
-  data(): { listItems: Item[] } {
+  data(): { listItems: Character[], info: Info } {
     return {
-      listItems: []
+      listItems: [],
+      info: {
+        count: 0,
+        pages: 0,
+        next: '',
+        prev: '',
+      },
     }
   },
   methods: {
-    async fetchingCharacters() {
-      try {
-        const {results} = await di.character.getAllCharacter()
-        this.listItems = results;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  },
-  mounted() {
-    this.fetchingCharacters();
-  }
+     async fetchCharacters() {
+       try {
+         const {results, info} = await di.character.getAllCharacter()
+         this.listItems = results;
+         this.info = info;
+       } catch (error) {
+         console.error(error);
+       }
+     }
+   },
+   mounted() {
+     this.fetchCharacters();
+   }
 }
 </script>
 
