ec2b430d7c17c4555d84699e86e3cf9d6b8d7793
rakamahardika <rakamahardika22@gmail.com>
2023-12-08 07:14:04 +0700

    checkpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import '@mdi/font/css/materialdesignicons.css'
 import './assets/main.css'
 import 'vuetify/styles'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
